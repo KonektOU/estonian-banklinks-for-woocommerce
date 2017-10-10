e78270db32f760c6e29c90847f34fd9dfe34a274
Risto Niinemets <risto.niinemets@gmail.com>
2017-10-10 11:20:51 +0300

    rename nordea to luminor

--- includes/gateways/class-wc-banklink-nordea-ipizza-gateway.php
+++ includes/gateways/class-wc-banklink-nordea-ipizza-gateway.php
@@ -5,8 +5,8 @@ class WC_Banklink_Nordea_Ipizza_Gateway extends WC_Banklink_Ipizza {
 	 */
 	function __construct() {
 		$this->id                 = 'nordea_ipizza';
-		$this->method_title       = __( 'Nordea', 'wc-gateway-estonia-banklink' );
-		$this->method_description = __( 'Nordea payment gateway via IPIZZA protocol. Use for new clients.', 'wc-gateway-estonia-banklink' );
+		$this->method_title       = __( 'Luminor', 'wc-gateway-estonia-banklink' );
+		$this->method_description = __( 'Luminor (former Nordea) payment gateway via IPIZZA protocol. Use for new clients.', 'wc-gateway-estonia-banklink' );
 
 		$this->encoding           = 'UTF-8';
 
