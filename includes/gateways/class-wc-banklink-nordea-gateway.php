1d9dab936d21b89b8f465cdc6b7b7e0c7a628c24
Risto Niinemets <risto.niinemets@gmail.com>
2017-10-10 11:21:17 +0300

    add gitignore

--- includes/gateways/class-wc-banklink-nordea-gateway.php
+++ includes/gateways/class-wc-banklink-nordea-gateway.php
@@ -4,7 +4,7 @@ class WC_Banklink_Nordea_Gateway extends WC_Banklink_Solo {
 	 * WC_Banklink_Nordea_Gateway
 	 */
 	function __construct() {
-		$this->id           = 'nordea';
+		$this->id                 = 'nordea';
 		$this->method_title       = __( 'Luminor', 'wc-gateway-estonia-banklink' );
 		$this->method_description = __( 'Luminor (former Nordea) payment gateway via SOLO protocol. Use for older clients.', 'wc-gateway-estonia-banklink' );
 
